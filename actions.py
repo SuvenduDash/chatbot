54f4abd321c7e9a373064c661892a6bf571b854b
Suvendu Kumar Dash <SuvenduDash@users.noreply.github.com>
2019-11-23 13:56:02 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,32 +1,5 @@
 # This files contains your custom actions which can be used to run
 # custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message("Hello World!")
-#
-#         return []
-# This files contains your custom actions which can be used to run
-# custom Python code.
 from rasa_sdk.actions import Action
 import requests
 from rasa_sdk import Action, Tracker
@@ -59,7 +32,7 @@ class FetchTypeOfServices(Action):
 
 	def run(self,dispatcher,tracker,domain):
 		service_type = tracker.get_slot('type_of_services')
-		service_link = "http://oretes.com/services/"
+		service_link = "http://ocpl.com/services/"
 		response = "We are providing {} services. You can get more info on {} from {}".format(service_type,service_type,service_link)
 		dispatcher.utter_message(response)
 		return [SlotSet('type_of_services', service_type)]
